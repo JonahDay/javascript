b41037e51605c5a298c5676edb2cfb9eb5d14594
Jonah Day <dayjm@school.hopeseattle.org>
2022-01-24 12:29:25 -0800

    jscript

---
+++ index.js
@@ -0,0 +1,24 @@
+const TextValue="Hello World";
+console.log(TextValue.toUpperCase());
+var total=0 ;
+console.log('Total:', total);
+total=10
+console.log('New Total:', total)
+const myarry=[2, 4, 6, 8, 10];
+console.log("My Array:", myarry[3]);
+let txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let length = txt.length;
+console.log("Length:", length);
+let str = "Wood, Stone, Metal";
+let part = str.slice(0,18);
+console.log("String:", part);
+let text = "Gibby mains suck!";
+let newText = text.replace("Gibby", "Wraith")
+console.log("NewString:", newText)
+let text1 = "Hello"
+let text2 = "World"
+let text3 = text1.concat(" ", text2)
+console.log("Concat:", text3)
+let text4 = "             Hello World              "
+let text5 = text4.trim()
+console.log("Trim:", text5)
